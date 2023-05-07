7863bbd8bf4d6430096733549c96cccffe029346
mocchantai <mocchantai31415@gmail.com>
2023-05-07 20:18:39 +0900

    add named-component slot

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,10 +1,12 @@
 <x-app-layout>
+{{--    layoutsのappのコンポーネントの{{ $header }}に挿入される--}}
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Dashboard') }}
         </h2>
     </x-slot>
 
+{{--    layoutsのappのコンポーネントの{{ $slot }}に挿入される--}}
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
