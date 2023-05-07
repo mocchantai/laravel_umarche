d947f7b51ef7526113f384566c5475827d5f31bd
mocchantai <mocchantai31415@gmail.com>
2023-05-07 20:37:26 +0900

    add component attribute bag ->merge class

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -6,5 +6,6 @@
 
     <x-tests.card title="タイトル1" content="コンテンツ1" :message="$message" />
     <x-tests.card title="タイトル２" />
+    <x-tests.card title="CSSを変更したい" class="bg-red-300" />
 </x-tests.app>
 
